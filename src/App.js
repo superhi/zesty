b04ebf2edec91fd1c48e309b2307fb9afbb731ce
isabelxklee <hi@kleetime.com>
2021-06-15 14:28:58 -0400

    added navigation arrow feature

--- src/App.js
+++ src/App.js
@@ -6,12 +6,14 @@ import fig from "./assets/image3.jpeg";
 import gaze from "./assets/image4.jpeg";
 import peach from "./assets/image5.jpeg";
 import avocado from "./assets/image6.jpeg";
+import { ReactComponent as BackArrow } from "./assets/arrow-left.svg";
+import { ReactComponent as NextArrow } from "./assets/arrow-right.svg";
 
 const images = [cabbage, mango, fig, gaze, peach, avocado];
 
 const Loading = ({ calculatedWidth }) => (
   <aside>
-    <div className="loading-bar">
+    <div className="loading">
       <label htmlFor="images-loaded">Loading all your favorite images...</label>
       <progress id="images-loaded" max="100" value={calculatedWidth}></progress>
     </div>
@@ -22,11 +24,13 @@ const App = () => {
   const [currentImage, setCurrentImage] = useState(0);
   const [numLoaded, setNumLoaded] = useState(0);
 
-  const handleClick = () => {
-    const length = images.length - 1;
+  const handleNext = () => {
+    setCurrentImage((currentImage) => (currentImage + 1) % images.length);
+  };
 
-    setCurrentImage((currentImage) =>
-      currentImage < length ? currentImage + 1 : 0
+  const handleBack = () => {
+    setCurrentImage(
+      (currentImage) => (currentImage - 1 + images.length) % images.length
     );
   };
 
@@ -35,33 +39,38 @@ const App = () => {
   };
 
   return (
-    <section>
-      <header>
-        <h1>Zesty</h1>
-        <h2>
-          A photography project <br /> by Ella Fieldling
-        </h2>
-      </header>
-
-      <figure>
-        {numLoaded < images.length && (
-          <Loading calculatedWidth={(numLoaded / images.length) * 100} />
-        )}
-        <figcaption>
-          {currentImage + 1} / {images.length}
-        </figcaption>
-        {images.map((imageURL, index) => (
-          <img
-            alt=""
-            key={imageURL}
-            src={imageURL}
-            onClick={handleClick}
-            onLoad={handleImageLoad}
-            className={currentImage === index ? "display" : "hide"}
-          />
-        ))}
-      </figure>
-    </section>
+    <>
+      <section>
+        <header>
+          <h1>Zesty</h1>
+          <h2>
+            A photography project <br />
+            by Ella Fieldling
+          </h2>
+        </header>
+        <figure>
+          {numLoaded < images.length && (
+            <Loading calculatedWidth={(numLoaded / images.length) * 100} />
+          )}
+          <nav>
+            <BackArrow onClick={handleBack} />
+            <NextArrow onClick={handleNext} />
+          </nav>
+          <figcaption>
+            {currentImage + 1} / {images.length}
+          </figcaption>
+          {images.map((src, index) => (
+            <img
+              alt=""
+              onLoad={handleImageLoad}
+              key={src}
+              src={src}
+              style={{ opacity: currentImage === index ? 1 : 0 }}
+            />
+          ))}
+        </figure>
+      </section>
+    </>
   );
 };
 
